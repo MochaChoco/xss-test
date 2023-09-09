53a2ae1f5fd93acb220228f96c4002a063118fb5
소원진 <swj9077@gmail.com>
2023-09-09 23:25:01 +0900

    reflected xss 재현 방법 추가

---
+++ src/views/main.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const script = route?.query?.script ?? '';
+</script>
+
+<template>
+  <div>
+    <div id="target">{{ script }}</div>
+  </div>
+</template>
